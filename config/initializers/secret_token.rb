c41be013e09cf8ceda12159e9ecfe31314060552
Baisemain <baisemain@online.de>
2011-05-25 14:32:36 +0200

    fixes #140:generated new application with rails_apps_composer

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maktaba::Application.config.secret_token = '601eeec68c15454f0ca01bcc283d60e14be4042ea5db3a15217c0b2498e5e6b8b840d9de0231284ac42474a76cc6e94e994098cb3e4ecd9bb3455b3725d10e89'
